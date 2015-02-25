0a92fd04e9bf0d0de2eb47b0b6ea383057ce8921
Christine Pham <cstinepham@gmail.com>
2015-02-25 11:43:33 -0800

    Added getting living room temp and occupancy functions

--- SoDecApp/app/src/main/java/edu/calpoly/sodec/sodecapp/ServerConnection.java
+++ SoDecApp/app/src/main/java/edu/calpoly/sodec/sodecapp/ServerConnection.java
@@ -18,7 +18,9 @@ public class ServerConnection {
     //private static final String BASE_SERVER_URI = "http://192.227.237.2:3000";
     // Local backend when using an emulator
     private static final String BASE_SERVER_URI = "http://10.0.2.2:3000";
-
+    private static final String POWER_ROUTE = "/power";
+    private static final String LR_TEMP_ROUTE = "/s-temp-lr";
+    private static final String LR_OCC_ROUTE = "/s-occ-lr";
     public static final int SUCCESS = 200;
 
     // For now we do not need to associate any state with this class. Some will probably be added
@@ -70,4 +72,58 @@ public class ServerConnection {
             }
         }.execute();
     }
+
+    /** Retrieve the living room temperature */
+    public void getLivingRoomTemp(final ResponseCallback<String, String> onSuccess) {
+        connect(onSuccess, LR_TEMP_ROUTE);
+    }
+
+    /** Retrieve the living room occupancy */
+    public void getLivingRoomOcc(final ResponseCallback<String, String> onSuccess) {
+        connect(onSuccess, LR_OCC_ROUTE);
+    }
+
+
+
+    private void connect(final ResponseCallback<String, String> onSuccess, final String route) {
+        new AsyncTask<Void, Void, Map>() {
+
+            @Override
+            protected Map doInBackground(Void... params) {
+
+                HttpGet request = new HttpGet(BASE_SERVER_URI + route);
+                CloseableHttpClient httpClient = HttpClients.createDefault();
+                CloseableHttpResponse response = null;
+                Map<String, String> jsonResponse = null;
+
+                try {
+                    response = httpClient.execute(request);
+
+                    if (response.getStatusLine().getStatusCode() == SUCCESS) {
+                        jsonResponse = new ObjectMapper().readValue(
+                                response.getEntity().getContent(), Map.class);
+                    }
+                    else {
+                        /* Only 23 characters allowed */
+                        Log.d("ServerConnect " + route, "Error: " +
+                                response.getStatusLine().getStatusCode());
+                    }
+                    response.close();
+                    httpClient.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return jsonResponse;
+            }
+
+            @Override
+            protected void onPostExecute(Map response) {
+                if (response != null) {
+                    onSuccess.execute(response);
+                }
+            }
+        }.execute();
+    }
 }
+
